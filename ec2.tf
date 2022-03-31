1c05e39de771f99ca3caf292468dbe518494dcef
Carsten Wiesbaum <carsten@wiesbaum.org>
2022-03-31 14:21:13 +0200

    Split up terraform scripts into multiple files
    ec2.tf:
    - File for ec2 configurations
    - Added specific availability_zone (eu-central-1a)
    - Added root_block_device configuration
    
    main.tf:
    - Basic configuration

---
+++ ec2.tf
@@ -0,0 +1,38 @@
+data "template_cloudinit_config" "config" {
+  gzip          = true
+  base64_encode = true
+
+  part {
+    filename     = "cloud-init.yaml"
+    content_type = "text/cloud-config"
+    content      = templatefile(
+        "./scripts/cloud-init.yaml",
+        {
+          ssh_public_key = file(var.ssh_public_key_location),
+          murmur_su_password = var.murmur_su_password,
+          murmur_config_gzipb64 = base64gzip(file(var.murmur_config_location))
+        }
+      )
+  }
+}
+
+resource "aws_instance" "murmur-server" {
+  ami           = "ami-05983e3aff3bf44ba"
+  instance_type = "t2.micro"
+  availability_zone = "eu-central-1a"
+  user_data_base64 = "${data.template_cloudinit_config.config.rendered}"
+
+  root_block_device {
+    delete_on_termination = false
+    encrypted = false
+    volume_size = 2
+    volume_type = "gp2"
+    tags = {
+        Name = "MurmurServer"
+    }      
+  }
+
+  tags = {
+    Name = "MurmurServer"
+  }
+}
--- main.tf
+++ main.tf
@@ -11,31 +11,3 @@ provider "aws" {
   profile = "default"
   region  = "eu-central-1"
 }
-
-data "template_cloudinit_config" "config" {
-  gzip          = true
-  base64_encode = true
-
-  part {
-    filename     = "cloud-init.yaml"
-    content_type = "text/cloud-config"
-    content      = templatefile(
-        "./scripts/cloud-init.yaml",
-        {
-          ssh_public_key = file(var.ssh_public_key_location),
-          murmur_su_password = var.murmur_su_password,
-          murmur_config_gzipb64 = base64gzip(file(var.murmur_config_location))
-        }
-      )
-  }
-}
-
-resource "aws_instance" "murmur-server" {
-  ami           = "ami-05983e3aff3bf44ba"
-  instance_type = "t2.micro"
-  user_data_base64 = "${data.template_cloudinit_config.config.rendered}"
-
-  tags = {
-    Name = "MurmurServer"
-  }
-}
